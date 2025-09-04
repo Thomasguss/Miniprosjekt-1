c8f37101db5b5655da0384aed9d68bc0b044b014
Thomasguss <thomas.gussgard@hotmail.com>
2025-09-04 16:29:16 +0200

    Jeg har laget et vindu

--- Lektion 1/draw.py
+++ Lektion 1/draw.py
@@ -4,11 +4,14 @@ pygame.init() # Initialize Pygame
 screen = pygame.display.set_mode((640, 480)) # Create a window of 640x480 pixels
 screen.fill((255, 255, 255)) # Fill the screen with white
 
-pygame.draw.line(screen, (0, 0, 0), (100, 380), (540, 380)) # Draw a black line
-pygame.draw.line(screen, (0, 0, 0), (100, 380), (100, 100))
-pygame.draw.line(screen, (0, 0, 0), (100, 100), (320, 50) )
-pygame.draw.line(screen, (0, 0, 0), (320, 50), (540, 100) )
-pygame.draw.line(screen, (0, 0, 0), (540, 380), (540, 100) )
+pygame.draw.line(screen, (150, 0, 150), (100, 380), (540, 380)) # Draw a black line
+pygame.draw.line(screen, (70, 60, 100), (100, 380), (100, 100)) # wall 1
+pygame.draw.line(screen, (100, 100, 100), (100, 100), (320, 50) ) # Roof 1
+pygame.draw.line(screen, (0, 0, 200), (320, 50), (540, 100) ) # Roof 2
+pygame.draw.line(screen, (200, 0, 0), (540, 380), (540, 100) )# Wall 2
+pygame.draw.line(screen, (0, 200, 0), (50, 382), (600, 382) ) #ground
+pygame.draw.circle(screen, (0, 0, 200), (200, 200), radius= 50, width=1) 
+
 # Draw the ground
 # Draw the bottom of the house
 # Draw two walls
